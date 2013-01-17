378af4c45bba2700d2b8641e325a0e2de412dd56
westeast <510578774@qq.com>
2013-01-17 11:34:37 +0800

    获得客户端ip函数getIp()

--- php/network/common.funcs.php
+++ php/network/common.funcs.php
@@ -1,1 +1,24 @@
 <?php
+/**
+ * 获取客户端ip
+ */
+function getIp(){
+	$ip = false;
+	if (!empty(getenv('HTTP_CLIENT_IP'))){
+		$ip = getenv('HTTP_CLIENT_IP');
+	}
+	if (!empty(getenv('HTTP_X_FORWARDED_FOR'))){
+		$ips = explode(',', getenv('HTTP_X_FORWARDED_FOR'));
+		if ($ip){
+			array_unshift($ips, $ip);
+			$ip = false;
+		}
+		for ($i = 0,$nums = count($ips); $i < $nums; $i++){
+			if (!preg_match('^(10|172\.16|192\.168)\.', $ips[$i])){
+				$ip = $ips[$i];
+				break;
+			}
+		}
+	}
+	return ($ip ? $ip : getenv('REMOTE_ADDR'));//可能 是客户端ip也可能是代理的ip不能伪造
+}
