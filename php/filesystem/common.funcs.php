4694d2a20da4d94a194122510446e88e52cb0ce5
westeast <510578774@qq.com>
2013-04-20 10:38:14 +0800

    read_folder函数

--- php/filesystem/common.funcs.php
+++ php/filesystem/common.funcs.php
@@ -2,3 +2,22 @@
 /**
  * 文件操作有关的常用的函数
  */
+ /**
+  * Return an array of file names and folders in directory;
+  */
+ function read_folder( $dir = '/etc'){
+	 $listDir = array();
+	 if($handler = opendir($dir)){
+		 while(($sub = readdir($handler)) != false ){
+			 if($sub != "." && $sub != ".."){
+				 if(is_file($dir."/".$sub)){
+					 $listDir[] = $sub;
+				 }elseif(is_dir($dir.'/'.$sub)){
+					 $listDir[$sub] = read_folder($dir.'/'.$sub);
+				 }
+			 }
+		 }
+		 closedir($handler);
+	 }
+	 return $listDir;
+ }
